88aea56e3f69ab33b917f679846569443441e1c5
paulo <pbrsj1385@gmail.com>
2022-12-14 11:33:04 -0300

    inicia projeto

---
+++ src/main/java/br/com/amordepet/banhotosa/BanhoTosaApplication.java
@@ -0,0 +1,13 @@
+package br.com.amordepet.banhotosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhoTosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhoTosaApplication.class, args);
+	}
+
+}
