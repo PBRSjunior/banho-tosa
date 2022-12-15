a8288112c9042ae9d37ab409b314b21d3d7e9740
paulo <pbrsj1385@gmail.com>
2022-12-15 10:44:01 -0300

    implementa API teste

--- src/main/java/br/com/amordepet/banhotosa/BanhoTosaApplication.java
+++ src/main/java/br/com/amordepet/banhotosa/BanhoTosaApplication.java
@@ -2,10 +2,19 @@ package br.com.amordepet.banhotosa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BanhoTosaApplication {
 
+	@GetMapping
+	public String gethomeTeste() {
+		return "Banho tosa - API Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BanhoTosaApplication.class, args);
 	}
