ae7cd43e0500f51d63da72eda648ab9d2bfefbb7
paulo <pbrsj1385@gmail.com>
2022-12-29 10:50:21 -0300

    implementa request e response

--- src/main/java/br/com/amordepet/banhotosa/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/amordepet/banhotosa/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,19 @@
 package br.com.amordepet.banhotosa.cliente.application.api;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	private String celular;
+	@NotBlank
+	private String endereco;
+	@CPF
+	private String cpf;
 
 }
